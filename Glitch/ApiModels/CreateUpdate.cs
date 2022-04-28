098e6b012d446aebf48d579e49c145898536d9c0
yiksvolro <oleksandr.orlovskii.pm.2020@lpnu.ua>
2022-04-28 10:46:11 +0300

    base service created

---
+++ Glitch/ApiModels/CreateUpdate.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Glitch.ApiModels
+{
+    public class CreateUpdate<T>
+    {
+        public bool Success { get; set; }
+
+        public T Model { get; set; }
+
+        public ModelStateDictionary Errors { get; set; }
+    }
+}
---
+++ Glitch/Services/Interfaces/IBaseService.cs
@@ -0,0 +1,16 @@
+﻿using Glitch.ApiModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Glitch.Services.Interfaces
+{
+    public interface IBaseService<T>
+    {
+        Task<CreateUpdate<T>> CreateAsync(T model);
+        Task<List<CreateUpdate<T>>> CreateRangeAsync(List<T> models);
+        Task<CreateUpdate<T>> UpdateAsync(T model);
+        Task<T> GetByIdAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task<bool> DeleteAsync(int id);
+    }
+}
