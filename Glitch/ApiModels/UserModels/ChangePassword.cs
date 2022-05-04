05d7c7db97067f575e8edb66c5544088faafffc2
Voitkiv-Dmytro <dmytro.voitkiv.pm.2020@lpnu.ua>
2022-05-04 11:53:15 +0300

    add apimodels

---
+++ Glitch/ApiModels/UserModels/ChangePassword.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Glitch.ApiModels.UserModels
+{
+    public class ChangePassword
+    {
+        public string Email { get; set; }
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
---
+++ Glitch/ApiModels/UserModels/LoginUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Glitch.ApiModels.UserModels
+{
+    public class LoginUser
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
