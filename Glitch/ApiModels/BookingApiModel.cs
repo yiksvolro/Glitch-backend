fb253bacd2305c293d7cd1389370dd4ae3bdf6c0
Voitkiv-Dmytro <dmytro.voitkiv.pm.2020@lpnu.ua>
2022-05-18 10:58:34 +0300

    add apimodel

---
+++ Glitch/ApiModels/BookingApiModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+public class BookingApiModel
+{
+	public BookingApiModel()
+	{
+        public string Id { get; set; }
+        public int UserId { get; set; }
+        public int PlaceId { get; set; }
+        public int TableId { get; set; }
+        public DateTime BookedOn { get; set; }
+    }
+}
