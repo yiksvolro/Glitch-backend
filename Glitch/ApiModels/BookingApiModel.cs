cfdb1b6f55a2e95b7ade3f2a929b394fac0cf203
Voitkiv-Dmytro <dmytro.voitkiv.pm.2020@lpnu.ua>
2022-05-18 11:05:09 +0300

    change apimodel

--- Glitch/ApiModels/BookingApiModel.cs
+++ Glitch/ApiModels/BookingApiModel.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-
-public class BookingApiModel
+namespace Glitch.ApiModels
 {
-	public BookingApiModel()
-	{
+    public class BookingApiModel
+    {
         public string Id { get; set; }
         public int UserId { get; set; }
         public int PlaceId { get; set; }
